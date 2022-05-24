844c7d363bf21fb194ef9d4e163e503c443b9919
Lam Duc Nguyen <ndl@es.aau.dk>
2022-05-24 10:47:55 -0400

    commit

---
+++ src/config.py
@@ -0,0 +1,7 @@
+# Dictionary storing network parameters.
+params = {
+    'num_epochs': 100,# Number of epochs to train for.
+    'learning_rate': 2e-4,# Learning rate.
+    'beta1': 0.5,
+    'save_epoch' : 10,# After how many epochs to save checkpoints and generate test output.
+}
